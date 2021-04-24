6ad4386d156bb151e57d51b4d50f09fee929aefb
ZawadiSumba66 <rosesumba98@gmail.com>
2021-04-24 18:01:20 +0300

    Style layout of the weather appp

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-import {weatherOrigin, weatherParameters} from './weather.js'
+import {weatherOrigin, weatherParameters,showWeather} from './weather.js'
 import './style.css';
-const content = document.querySelector('#content')
-const form = document.createElement('form')
-const searchBox = document.createElement('input')
-searchBox.setAttribute('id', `search`);
-searchBox.type = 'search';
-searchBox.placeholder = 'Enter city location'
-const units = 'metric';
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const weatherData = searchBox.value
-    form.reset();
-    weatherOrigin(weatherData)
-    .then((data)=>{weatherParameters(data,units)
-    })
-})
+
+showWeather()
 
 window.onload = async()=>{
-    weatherOrigin('london').then((data)=>{
-        weatherParameters(data,units) 
+    weatherOrigin('Nairobi').then((data)=>{
+        weatherParameters(data) 
     })
 }
-searchBox.value = ""
-form.appendChild(searchBox)
-content.appendChild(form);
+
 weatherParameters()
--- src/weather.js
+++ src/weather.js
@@ -1,26 +1,32 @@
-import weather from './assets/bg-weather.png';
+import weather from './assets/bg-weather.jpg';
 const content = document.querySelector('#content')
 const button = document.createElement('button')
 const locationDegree = document.createElement('div')
 const date = document.createElement('div')
-const humid = document.createElement('div')
-const pressure = document.createElement('div')
 const description = document.createElement('div')
 const cityName = document.createElement('h3')
+const humidityH = document.createElement('div')
+const humid = document.createElement('div')
+const humidP = document.createElement('div')
+const windH = document.createElement('div')
 const wind = document.createElement('div')
-// const showcase = () =>{
-//     const main = document.createElement('div');
-//     main.classList.add('showcase')
-//     main.innerHTML = 'i love you'
-//     main.style.background = `url(${weather})`;
-//     main.style.backgroundRepeat = 'no-repeat';
-//     main.style.backgroundSize = 'cover';
-//     const weatherCard = document.createElement('div')
-//     content.appendChild(main);
-// }
+const windP = document.createElement('div')
+const pressureH = document.createElement('div')
+const pressure = document.createElement('div')
+const pressureP = document.createElement('div')
+const icon = new Image()
+const allHumid = document.createElement('div')
+const allPressure = document.createElement('div')
+const allWind = document.createElement('div')
+const main = document.createElement('div');
+main.classList.add('showcase','text-center')
+main.style.background = `url(${weather})`;
+main.style.backgroundRepeat = 'no-repeat';
+main.style.backgroundSize = 'cover';
+
 
 const weatherOrigin = async (city)=>{
-    let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units='metric'&APPID=71fbfe5f66beeee255c8447a6ae9b37b`);
+    let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=71fbfe5f66beeee255c8447a6ae9b37b`);
     const data = await response.json();
     if (data.cod === 200) {
         return data;
@@ -28,51 +34,82 @@ const weatherOrigin = async (city)=>{
 }
 
 const weatherParameters= async(data,units)=>{
-    const ul = document.createElement('ul')
-    const li = document.createElement('li')
-    li.innerHTML=''
+    const all = document.createElement('div')
+   
+    const component = document.createElement('div')
+    component.innerHTML=''
     const temperature = data.main.temp
-    // document.createElement('div')
-    // temperature.textContent = data.main.temp
     date.innerHTML = new Date()
     locationDegree.innerHTML = `${temperature}&degC`
-     humid.textContent = data.main.humidity
-     pressure.textContent = data.main.pressure
+    locationDegree.classList.add('temperature')
+    const header = document.createElement('div')
+    header.classList.add('d-flex','justify-content-between')
     description.textContent = data.weather[0].description
-     const icon = document.createElement('div')
-     icon.textContent = data.weather.icon
+    description.classList.add('text-center','description')
+    const climate = data.weather[0].icon
+    icon.src =`https://openweathermap.org/img/wn/${climate}.png`
+    
+    //  icon.innerHTML =  `<img src="https://openweathermap.org/img/wn/${climate}.png`
     cityName.textContent = data.name
+    cityName.classList.add('text-center')
+    const div = document.createElement('div')
+    div.classList.add('justify-content-between','d-flex','measures')
+   
+    humidityH.textContent = 'Humidity'
+    humid.textContent = data.main.humidity
+    humidP.innerHTML='%'
+   
+    pressureH.textContent = 'Pressure'
+    pressure.textContent = data.main.pressure
+    pressureP.innerHTML='hPa'
+
+    windH.innerHTML='Wind'
      wind.innerHTML = data.wind.speed
+     windP.innerHTML='m/s'
+     allHumid.appendChild(humidityH)
+     allHumid.appendChild(humid)
+     allHumid.appendChild(humidP)
+     allPressure.appendChild(pressureH)
+     allPressure.appendChild(pressure)
+     allPressure.appendChild(pressureP)
+     allWind.appendChild(windH)
+     allWind.appendChild(wind)
+     allWind.appendChild(windP)
+    div.appendChild(allHumid)
+    div.appendChild(allPressure)
+    div.appendChild(allWind)
      button.innerHTML= 'Farenheit'
+     button.classList.add('d-flex','justify-content-end','btn','text-dark','bg-light','font-weight-bold')
      const tempF = (temperature * 1.8 + 32).toFixed(2);;
      const tempC = temperature;
      button.addEventListener('click',changeTemperature(temperature,tempF,tempC,units))
-     li.appendChild(button)
-     li.appendChild(cityName)
-     li.appendChild(date)
-     li.appendChild(locationDegree)
-     li.appendChild(humid)
-     li.appendChild(pressure)
-     li.appendChild(description)
-     li.appendChild(icon)
-     li.appendChild(wind)
-     ul.appendChild(li)
-     content.appendChild(ul)
+     header.appendChild(cityName)
+     header.appendChild(icon)
+    component.appendChild(header)
+    component.appendChild(date)
+    component.appendChild(description)
+    component.appendChild(locationDegree)
+    component.appendChild(div)
+    component.appendChild(button)
+    component.classList.add('parameters','px-5')
+     all.appendChild(component)
+     main.appendChild(all)
+     content.appendChild(main)
 }
 const changeTemperature =(temperature,tempF,tempC,units) =>{
     button.addEventListener('click',(e)=>{
         e.preventDefault();
-        const elm = e.target;
+        const element = e.target;
         if (units === 'imperial') {
           temperature = tempC;
           locationDegree.innerHTML = `${temperature}&degC`;
           units = 'metric';
-          elm.innerHTML = 'Fahrenheit';
+          element.innerHTML = 'Fahrenheit';
         } else {
           temperature = tempF;
           locationDegree.innerHTML = `${temperature}&degF`;
           units = 'imperial';
-           elm.innerHTML = 'Celcius';
+           element.innerHTML = 'Celcius';
         }
     })
     
@@ -83,29 +120,19 @@ const showWeather= ()=>{
     searchBox.setAttribute('id', `search`);
     searchBox.type = 'search';
     searchBox.placeholder = 'Enter city location'
+    searchBox.classList.add('form-control','w-50')
     form.addEventListener('submit',(e)=>{
         e.preventDefault();
         const weatherData = searchBox.value
         form.reset();
         weatherOrigin(weatherData)
         .then((data)=>{weatherParameters(data)
-        })
+        }).catch(err=>console.log(err))
     })
     searchBox.value = ""
     form.appendChild(searchBox)
-    content.appendChild(form);
+    main.appendChild(form);
+    content.appendChild(main)
 }
-
-
-
-// const createWeather = ()=>{
-//     const searchBox = document.createElement('input')
-//     searchBox.setAttribute('id', `search`);
-//     searchBox.type = 'search';
-//     searchBox.placeholder = 'Enter location'
-//     content.appendChild(searchBox);
-// }
-
-
 
 export {weatherOrigin,showWeather,weatherParameters,changeTemperature}
