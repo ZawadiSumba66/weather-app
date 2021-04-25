c313a42d5c21565d88969289224783fce08a3166
ZawadiSumba66 <rosesumba98@gmail.com>
2021-04-25 16:24:17 +0300

    Set the mode of the production and development

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
